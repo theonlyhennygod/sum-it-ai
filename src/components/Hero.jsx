fdbc5764e9efd9760b185b411c86fc0df960c3fd
Argenis De La Rosa <theonlyhennygod@gmail.com>
2024-05-13 13:56:07 -0400

    added logo and fixed navigation bar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { logo } from '../assets'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='w-full flex justify-between items-center p-4'>
+        <img src={logo} alt='logo' className='w-28 object-contain' />
+
+        <div className='flex justify-between items-center gap-4'>
+          <button type='button' onClick={() => window.open('https//www.github.com/theonlyhennygod')} className='bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded'>GitHub</button>
+
+          <button type='button' onClick={() => window.open('https//www.linkedin.com/in/argenisdelarosa')} className='bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded'>LinkedIn</button>
+        </div>
+      </nav>
+
+      <h1 className='text-6xl font-bold text-center text-gray-800'>Welcome to our website</h1>
+
+    </header>
   )
 }
 
